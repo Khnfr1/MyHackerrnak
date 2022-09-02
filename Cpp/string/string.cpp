0aa0be7609fe1f8d60ad041e441e58f14dbcfd04
Khan Farhan <88178370+Khnfr1@users.noreply.github.com>
2022-09-02 22:17:07 +0530

    Update string.cpp

--- Cpp/string/string.cpp
+++ Cpp/string/string.cpp
@@ -1,15 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
-
+#include <algorithm>
+#include <string>
 using namespace std;
 
 int main() {
-    string a, b;
-    cin >> a;
-    cin >> b;
-    cout << a.size() << " " << b.size();
-    cout << a + b;
-    char c = a[0];
-    a[0] = b[0];
-    b[0] = c;
-    cout << a << " " << b;
+   // Complete the program
+    string a,b;
+    cin>>a>>b;
+    cout<<a.size()<<" "<<b.size()<<endl;
+    cout<<a+b<<endl;
+    char t=a[0];
+    a[0]=b[0];
+    b[0]=t;
+    cout<<a<<" "<<b;
+    return 0;
 }
